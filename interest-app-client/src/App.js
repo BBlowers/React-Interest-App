103baae10bda7ba6057bc8fdc84f89cde1b73987
Ben Blowers <benblowers@outlook.com>
2016-11-08 13:19:39 +0000

    Slider interest rate selector component made

--- interest-app-client/src/App.js
+++ interest-app-client/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 let idCounter = 0;
 let exchangeCounter = 0;
 
-// { rate.currencyCode === this.props.row ? (selected)}
-
-class ExchangeSelector extends Component {
+class CurrencySelector extends Component {
   render() {
     return (
       <select onChange={(event) => {
@@ -24,20 +22,66 @@ class ExchangeSelector extends Component {
   }
 }
 
+class MainCurrencySelector extends Component {
+  render() {
+    return (
+      <select onChange={(event) => {
+        this.props.store.dispatch({
+          type: 'CHANGE_MAIN_CURRENCY',
+          mainCurrencyCode: event.target.value
+        });
+      }} value={this.props.mainCurrencyCode}>
+        { this.props.rates.map(rate =>
+            <option value={ rate.currencyCode } key={rate.currencyCode}>{ rate.currencyCode }</option>
+        )}
+      </select>
+    )
+  }
+}
+
+class MainValueInput extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Enter value to calculate</h1>
+        <input type="value" onChange={(event) => {
+          this.props.store.dispatch({
+            type: 'CHANGE_MAIN_VALUE',
+            mainValue: event.target.value
+          });
+        }} value={this.props.mainValue} />
+      </div>
+    )
+  }
+}
+
+class MainInterestSlider extends Component {
+  render() {
+    return (
+      <div>
+        <h1>User slide to select interest rate</h1>
+        <input id="test" type="range" min="0" max="100" onChange={(event) => {
+          this.props.store.dispatch({
+            type: 'CHANGE_MAIN_INTEREST',
+            mainInterest: event.target.value
+          })
+        }} value={this.props.mainInterest}/>
+        <h2>{this.props.mainInterest}</h2>
+      </div>
+    )
+  }
+}
+
 export default class App extends Component {
   render() {
     const { store } = this.props; //Plan to change this...
-
-
     return (
       <div>
+        <MainValueInput store={store} mainValue={this.props.mainValue}/>
+        <MainInterestSlider store={store} mainInterest={this.props.mainInterest}/>
         <div>
-          <h1>Enter value to calculate</h1>
-          <input type="value"/>
-        </div>
-        <div>
-          <h1>User slide to select interest rate</h1>
-          <input id="test" type="range" min="0" max="100"/>
+          <h1>Select main currency</h1>
+          <MainCurrencySelector mainCurrencyCode={this.props.mainCurrencyCode} store={store} rates={this.props.exchangeRates}/>
         </div>
         <button onClick={() => {
           store.dispatch({
@@ -50,7 +94,7 @@ export default class App extends Component {
         <ul>
           { this.props.rows.map(row => 
             <li key={row.id}>
-              <ExchangeSelector store={store} row={row} rates={this.props.exchangeRates}/>
+              <CurrencySelector store={store} row={row} rates={this.props.exchangeRates}/>
               <p>{row.currencyCode}</p>
             </li>
           )}
--- interest-app-client/src/index.js
+++ interest-app-client/src/index.js
@@ -16,7 +16,7 @@ store.dispatch({
 
 const render = () => {
   ReactDOM.render(
-    <App rows={store.getState().rows} exchangeRates={store.getState().exchangeRates} store={store}/>, //Plan to change this
+    <App rows={store.getState().rows} exchangeRates={store.getState().exchangeRates} mainCurrencyCode={store.getState().mainCurrencyCode} mainInterest={store.getState().mainInterest} mainValue={store.getState().mainValue} store={store}/>, //Plan to change this
     document.getElementById('root')
   )
 };
